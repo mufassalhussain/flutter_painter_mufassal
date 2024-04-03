41c97c02d21e4735cd616c14b8fce02ca7931274
mufassalredsignal <hmufassal@gmail.com>
2024-04-03 16:48:31 +0500

    emoji restriction

--- lib/src/views/widgets/text_widget.dart
+++ lib/src/views/widgets/text_widget.dart
@@ -246,7 +246,10 @@ class EditTextWidgetState extends State<EditTextWidget>
                   .clamp(0, screenHeight)),
           child: Center(
             child: TextField(
-              inputFormatters: [CustomInputFormatter()],
+              inputFormatters: [
+                FilteringTextInputFormatter.deny(RegExp(
+                    '(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'))
+              ],
               decoration: const InputDecoration(
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.zero,
